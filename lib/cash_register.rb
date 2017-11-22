3ff71a1346141b65f589875d3aa8dfad4231f2bf
Brittany Hartmire <bkhartmire@gmail.com>
2017-11-22 01:06:10 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -21,7 +21,7 @@ class CashRegister
   end
 
   def apply_discount
-    if @discount == 0.0
+    if @discount == 0
       return "There is no discount to apply."
     else
       @total -= (@discount.to_f/100) * @total
